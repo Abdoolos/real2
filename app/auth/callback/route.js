6dc96def79a3c771e682eb48ffb4bc34864b8b75
Abdullah Alawiss <Ah2xx@hotmail.com>
2025-10-15 18:47:51 +0200

    fix: Add await for createClient in auth callback - Fixed 500 error

--- app/auth/callback/route.js
+++ app/auth/callback/route.js
@@ -7,7 +7,7 @@ export async function GET(request) {
   const origin = requestUrl.origin
 
   if (code) {
-    const supabase = createClient()
+    const supabase = await createClient()
     await supabase.auth.exchangeCodeForSession(code)
   }
 
